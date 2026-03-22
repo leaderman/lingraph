961444d8fc26f139a0bdfaeffe4a6324574297db
Leaderman <90623688059836-Leaderman@noreply.coze.cn>
2026-03-22 20:20:19 +0800

    Restored to '2fae426e226d7208352ad2ed9172181882f3d665'
    Coze-Restored-To: '2fae426e226d7208352ad2ed9172181882f3d665'
    Coze-Commit-Type: restore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -357,7 +357,7 @@ export default function Home() {
                   className="flex justify-center rounded-lg border border-slate-200 bg-white p-4 dark:border-slate-700 dark:bg-slate-800"
                 >
                   <div
-                    className="inline-block rounded border border-slate-200 dark:border-slate-700"
+                    className="inline-block rounded border border-slate-200 p-3 dark:border-slate-700"
                     dangerouslySetInnerHTML={{ __html: image.html }}
                   />
                 </div>
