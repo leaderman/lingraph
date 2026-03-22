fdadabf004d166b5c4af98ce5467d296aee853c8
Leaderman <90623688059836-Leaderman@noreply.coze.cn>
2026-03-22 10:02:43 +0800

    feat: 获取 token 查询参数

--- src/app/api/get_tmp_download_url/route.ts
+++ src/app/api/get_tmp_download_url/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const token = searchParams.get('token');
+
   return NextResponse.json({
     code: 200,
     msg: 'success',
