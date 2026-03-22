a67820d060bbab6a7793bdffab66b436e13667d8
Leaderman <90623688059836-Leaderman@noreply.coze.cn>
2026-03-22 09:41:39 +0800

    refactor: 移除 language 变量的默认值

--- src/lib/block-processor.ts
+++ src/lib/block-processor.ts
@@ -52,7 +52,7 @@ function ordered(block: any, config: BlockConfig) {
 
 function code(block: any, config: BlockConfig) {
   const codeContent = block.json.code?.elements?.[0]?.text_run?.content || '';
-  const language = block.json.code?.style?.language || '';
+  const language = block.json.code?.style?.language;
   const codeClass = language ? `language-${language}` : 'language-plaintext';
   block.html = `<div><pre><code class="${codeClass}">${codeContent}</code></pre></div>`;
 }
