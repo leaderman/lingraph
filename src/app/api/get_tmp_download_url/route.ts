7ecfc784ee68e5757a1dee58bf1ef7e6d18783a3
Leaderman <90623688059836-Leaderman@noreply.coze.cn>
2026-03-22 10:02:12 +0800

    feat: 添加 /api/get_tmp_download_url 接口

---
+++ src/app/api/get_tmp_download_url/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  return NextResponse.json({
+    code: 200,
+    msg: 'success',
+    data: '',
+  });
+}
