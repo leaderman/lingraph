550b209e588611fd1652637a303eaef9bd0849ab
Leaderman <90623688059836-Leaderman@noreply.coze.cn>
2026-03-22 19:36:19 +0800

    style: 将 width 样式移到带边框的 div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -278,11 +278,11 @@ export default function Home() {
                     </div>
 
                     {/* 3. HTML 渲染效果 */}
-                    <div style={{ width: imageWidth }}>
+                    <div>
                       <h4 className="mb-2 text-sm font-medium text-slate-600 dark:text-slate-400">
                         {block.block_name}
                       </h4>
-                      <div className="rounded border border-slate-200 p-3 dark:border-slate-700">
+                      <div className="rounded border border-slate-200 p-3 dark:border-slate-700" style={{ width: imageWidth }}>
                         <div
                           ref={(el) => { blockRefs.current[index] = el?.firstElementChild as HTMLDivElement; }}
                           dangerouslySetInnerHTML={{ __html: block.html }}
