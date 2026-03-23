076ef10cca69599a4d9c40a4d1cd7b8913055f36
Leaderman <90623688059836-Leaderman@noreply.coze.cn>
2026-03-23 15:47:23 +0800

    Restored to '49fca02f9b10e4a7e40e4fc7d35462bf26b2d73e'
    Coze-Restored-To: '49fca02f9b10e4a7e40e4fc7d35462bf26b2d73e'
    Coze-Commit-Type: restore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,6 @@ export default function Home() {
       setBlocks(newBlocks);
       setShouldMeasure(false);
       createImages(newBlocks);
-      setActiveTab('images');
     });
   }, [shouldMeasure, blocks]);
 
