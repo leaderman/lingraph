2a09ecf69319d1fe64fbd5f18710e0c9bfeedb01
Leaderman <90623688059836-Leaderman@noreply.coze.cn>
2026-03-24 08:40:28 +0800

    refactor: 日志文字改为处理

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
     });
 
     for (const [index, block] of blocks.entries()) {
-      console.log('插入第 ', index, ' 个块');
+      console.log('处理第 ', index, ' 个块');
       
       const blockHeight = block.json.block_height;
 
